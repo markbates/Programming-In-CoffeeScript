9eb9a63532feb98dc9808ae807e72a07ff58f8f4
Mark Bates <mark@markbates.com>
2011-11-29 18:17:31 -0500

    Updated Chapter 7 with Stuart's latest feedback.

---
+++ cake/example2a/Cakefile.js
@@ -0,0 +1,7 @@
+
+  option('-n', '--name [NAME]', 'name you want to greet');
+
+  task("greet", "Say hi to someone", function(options) {
+    if (options.name == null) throw new Error("[NAME] is required");
+    return console.log("Hello, " + options.name);
+  });
